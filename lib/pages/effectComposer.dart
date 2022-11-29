640aa247f92f3c4e33af6f9eeb8b745d1bcadb92
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-11-29 18:07:23 +0100

    can reorder effects

--- lib/pages/effectComposer.dart
+++ lib/pages/effectComposer.dart
@@ -36,48 +36,73 @@ class _ComposerState extends State<Composer> {
         leading: BackButton(),
         title: const Text('Composer'),
       ),
-      body: ListView(children: [
-        ...effects
-            .map((effect) => ExpansionTile(
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        effect.config.title,
-                        style: !effect.enabled
-                            ? const TextStyle(
-                                decoration: TextDecoration.lineThrough)
-                            : null,
-                      ),
-                      if (!effect.expanded)
-                        Expanded(child: effect.config.preview),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () => setState(() => effects.remove(effect)),
-                      ),
-                    ],
-                  ),
-                  onExpansionChanged: (value) =>
-                      setState(() => effect.expanded = value),
-                  children: [
-                    effect.config.editor,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () =>
-                              setState(() => effect.enabled = !effect.enabled),
-                          child: Text(effect.enabled ? 'Disable' : 'Enable'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ))
-            .toList(),
-        NewEffectButton(
-            onSelected: (config) =>
-                setState(() => effects.add(EffectState(config: config)))),
-      ]),
+      body: Column(
+        children: [
+          Expanded(
+            child: ReorderableListView(
+                onReorder: (oldIndex, newIndex) => setState(() {
+                      if (oldIndex < newIndex) {
+                        newIndex -= 1;
+                      }
+                      final EffectState item = effects.removeAt(oldIndex);
+                      effects.insert(newIndex, item);
+                    }),
+                children: [
+                  ...effects
+                      .map((effect) => ExpansionTile(
+                            controlAffinity: ListTileControlAffinity.leading,
+                            key: ValueKey(effect),
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  effect.config.title,
+                                  style: !effect.enabled
+                                      ? const TextStyle(
+                                          decoration:
+                                              TextDecoration.lineThrough)
+                                      : null,
+                                ),
+                                if (!effect.expanded)
+                                  Expanded(child: effect.config.preview),
+                                IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () =>
+                                      setState(() => effects.remove(effect)),
+                                ),
+                                const SizedBox(width: 20),
+                              ],
+                            ),
+                            onExpansionChanged: (value) =>
+                                setState(() => effect.expanded = value),
+                            children: [
+                              effect.config.editor,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    onPressed: () => setState(
+                                        () => effect.enabled = !effect.enabled),
+                                    child: Text(
+                                        effect.enabled ? 'Disable' : 'Enable'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                ]),
+          ),
+          NewEffectButton(
+            key: const ValueKey('newEffect'),
+            onSelected: (config) => setState(
+              () => effects.add(
+                EffectState(config: config),
+              ),
+            ),
+          ),
+        ],
+      ),
       endDrawer: const MainDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => widget.onSaved(effects
